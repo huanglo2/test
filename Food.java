37a1c242576018e0dfe61b0615420a70f91c4c9d
longsheng <906130@qq.com>
2017-09-20 10:42:11 +0800

    Initial commit

---
+++ Food.java
@@ -0,0 +1,26 @@
+package test;
+
+public class Food<K,V,L> {
+	public K k;
+	public V v;
+	public L l;
+	
+	public K getK() {
+		return k;
+	}
+	public void setK(K k) {
+		this.k = k;
+	}
+	public V getV() {
+		return v;
+	}
+	public void setV(V v) {
+		this.v = v;
+	}
+	public L getL() {
+		return l;
+	}
+	public void setL(L l) {
+		this.l = l;
+	}
+}
